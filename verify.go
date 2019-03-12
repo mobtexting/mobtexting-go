34e23851c53174bc36c5d2ec18ef8fef15cf7059
vishal8282 <vishalnishad888@gmail.com>
2019-03-12 15:35:36 +0530

    verify added

--- verify.go
+++ verify.go
@@ -1,5 +1,66 @@
 package mobtexting_sms
+import (
+                "net/http"
+                "io/ioutil"
+       )
 
-func Verify() string{
-	return "Hello verify"
+func VerifySend(to string, access_token string) string{
+        var api_endpoint = "https://portal.mobtexting.com/api/v2/"
+        var url = api_endpoint + "verify?to=" + to
+        req, err := http.NewRequest("POST", url, nil)
+        req.Header.Set("Authorization", "Bearer " + access_token)
+        req.Header.Set("Accept", "application/json")
+        client := &http.Client{}
+        resp, err := client.Do(req)
+
+        if err != nil {
+                return "request error!"
+        }
+        defer resp.Body.Close()
+
+        if resp.StatusCode == http.StatusOK {
+                bodyBytes, err2 := ioutil.ReadAll(resp.Body)
+                        bodyString := string(bodyBytes)
+                        if(err2 != nil){
+                                return "request error!!"
+                        }else{
+                                return bodyString
+                        }
+        }
+        return ""
+}
+
+func VerifyCheck(id string, token string, access_token string) string{
+        return doGet("verify/check/" + id + "/" + token, access_token)
+}
+
+func VerifyCancel(id string, access_token string) string{
+        return doGet("verify/cancel/" + id, access_token)
+}
+
+func doGet(url string, access_token string) string{
+        var api_endpoint = "https://portal.mobtexting.com/api/v2/"
+        url = api_endpoint + url
+
+        req, err := http.NewRequest("GET", url, nil)
+        req.Header.Set("Authorization", "Bearer " + access_token)
+        req.Header.Set("Accept", "application/json")
+        client := &http.Client{}
+        resp, err := client.Do(req)
+
+        if err != nil {
+                return "request error!"
+        }
+        defer resp.Body.Close()
+
+        if resp.StatusCode == http.StatusOK {
+                bodyBytes, err2 := ioutil.ReadAll(resp.Body)
+                        bodyString := string(bodyBytes)
+                        if(err2 != nil){
+                                return "request error!!"
+                        }else{
+                                return bodyString
+                        }
+        }
+        return ""
 }
